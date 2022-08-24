a18196e124fe76f0a08d80afefee84b75d054ae1
Shan Padayhag <shanpadayhag@gmail.com>
2022-08-24 11:25:30 +0800

    Added button

--- src/components/forms/buttons/Button.jsx
+++ src/components/forms/buttons/Button.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import styled from "styled-components";
+
+const ButtonComponent = styled.button`
+    background-color: var(--blue);
+    border: none;
+    border-radius: 10px;
+    height: 38px;
+    padding: 0 20px;
+    color: #fff;
+
+    font-size: 14px;
+    font-weight: 600;
+    text-transform: uppercase;
+`
 
 const Button = ({ children }) => {
-    return <button>{children}</button>
+    return <ButtonComponent>{children}</ButtonComponent>
 }
 
 export default Button;

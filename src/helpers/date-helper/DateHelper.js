7b07b4f07088eaab46c6ecfa4a03a6c9df6c7752
Shan Padayhag <shanpadayhag@gmail.com>
2022-08-25 15:27:58 +0800

    Added to return default value if date is invalid

--- src/helpers/date-helper/DateHelper.js
+++ src/helpers/date-helper/DateHelper.js
@@ -3,6 +3,10 @@ class DateHelper {
         const dateObject = new Date(dateString);
         const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
+        if (dateObject.toString() === 'Invalid Date') {
+            return dateString;
+        }
+
         const month = monthNames[dateObject.getMonth()];
         const date = dateObject.getDate();
         const year = dateObject.getFullYear();
